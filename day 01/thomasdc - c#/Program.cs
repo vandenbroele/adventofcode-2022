810d85670e3a20711c3ae36616b6331d59629cef
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-01 16:35:26 +0100

    thomasdc day 1

---
+++ day 01/thomasdc - c#/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine(File.ReadAllText("input.txt")
+    .Split("\r\n\r\n")
+    .Select(_ => _.Split("\r\n").Select(int.Parse).Sum())
+    .Max());
+
+Console.WriteLine(File.ReadAllText("input.txt")
+    .Split("\r\n\r\n")
+    .Select(_ => _.Split("\r\n").Select(int.Parse).Sum())
+    .OrderBy(_ => _)
+    .TakeLast(3)
+    .Sum());
