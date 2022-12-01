736b6f4e2181837ac6cdba9a34a7710a1e6eb495
Sander Decleer <sander.decleer@novado.be>
2022-12-01 16:50:20 +0100

    sanderdc Day1

---
+++ sanderdc - c#/Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿IEnumerable<string> lines = File.ReadLines("./input.txt");
+
+List<int> topThree = ElfCounter
+    .CreateCalorieCounts(lines)
+    .OrderDescending()
+    .Take(3)
+    .ToList();
+
+Console.WriteLine($"answer 1: {topThree.First()}");
+Console.WriteLine($"answer 2: {topThree.Sum()}");
+
+internal static class ElfCounter
+{
+    public static IEnumerable<int> CreateCalorieCounts(IEnumerable<string> lines)
+    {
+        int count = 0;
+        foreach (string line in lines)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                yield return count;
+                count = 0;
+            }
+            else
+            {
+                count += int.Parse(line);
+            }
+        }
+    }
+}
