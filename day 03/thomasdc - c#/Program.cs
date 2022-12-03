863b7c677b2a4a6a444e5d7298e3566ea84150e9
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-03 12:08:07 +0100

    thomasdc day 3

---
+++ day 03/thomasdc - c#/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine(
+    (
+        from line in File.ReadAllLines("input.txt")
+        let firstHalf = line[..(line.Length / 2)]
+        let secondHalf = line[^(line.Length / 2)..]
+        select firstHalf.Intersect(secondHalf))
+    .SelectMany(_ => _)
+    .Select(_ => (int) _)
+    .Sum(c => c switch
+    {
+        >= 'a' => c - 'a' + 1,
+        >= 'A' => c - 'A' + 27,
+        _ => 0
+    }));
+
+Console.WriteLine(File.ReadAllLines("input.txt").Chunk(3)
+    .Sum(group => (
+        from g in
+            from line in @group
+            from @char in line.Distinct()
+            let intChar = (int) @char
+            group intChar by intChar
+        where g.Count() == 3
+        select g.Key switch
+        {
+            >= 'a' => g.Key - 'a' + 1,
+            >= 'A' => g.Key - 'A' + 27,
+            _ => 0
+        }).Sum()));
