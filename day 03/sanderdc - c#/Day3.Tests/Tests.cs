2161232ab151be243bc7897b407e1f328947fdad
mrgelmir <sander.decleer@novado.be>
2022-12-03 13:23:02 +0100

    sanderdc Day3 part 1

---
+++ day 03/sanderdc - c#/Day3.Tests/Tests.cs
@@ -0,0 +1,26 @@
+namespace Day3.Tests;
+
+public class Tests
+{
+    [Test]
+    [TestCase('a', 1)]
+    [TestCase('z', 26)]
+    [TestCase('A', 27)]
+    [TestCase('Z', 52)]
+    public void Priority_Validate(char input, int expectedScore)
+    {
+        Assert.AreEqual(expectedScore, Helpers.GetCharPriority(input));
+    }
+
+    [Test]
+    [TestCase("vJrwpWtwJgWrhcsFMMfFFhFp", 'p')]
+    [TestCase("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", 'L')]
+    [TestCase("PmmdzqPrVvPwwTWBwg", 'P')]
+    [TestCase("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", 'v')]
+    [TestCase("ttgJtRGJQctTZtZT", 't')]
+    [TestCase("CrZsJsPPZsGzwwsLwLmpwMDw", 's')]
+    public void Overlap_Validate(string input, char expected)
+    {
+        Assert.AreEqual(expected, Helpers.GetOverlap(input));
+    }
+}
---
+++ day 03/sanderdc - c#/Day3/Program.cs
@@ -0,0 +1,29 @@
+﻿IEnumerable<string> lines = File.ReadLines("./input.txt");
+
+int count = lines
+    .Select(Helpers.GetOverlap)
+    .Sum(Helpers.GetCharPriority);
+
+Console.WriteLine(count);
+
+public static class Helpers
+{
+    public static char GetOverlap(string input)
+    {
+        int half = input.Length / 2;
+        string left = input[..half];
+        string right = input[half..];
+
+        return left.Intersect(right).First();
+    }
+
+    public static int GetCharPriority(char input)
+    {
+        if (char.IsUpper(input))
+        {
+            return 27 + input - 65;
+        }
+
+        return input - 96;
+    }
+}
