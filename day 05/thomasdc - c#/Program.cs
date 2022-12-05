d5fba5640e4388606511dc1cf566eab8911527a0
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-05 22:49:29 +0100

    thomasdc day 5 part 1

---
+++ day 05/thomasdc - c#/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines("input.txt");
+var height = lines
+    .Select((s, i) => (s, i))
+    .First(_ => _.s.StartsWith(" 1")).i;
+var stacks = Enumerable.Range(0, lines[height].Length / 4 + 1)
+    .Select(_ => new Stack<char>())
+    .ToArray();
+
+for (var y = height - 1; y >= 0; y--)
+{
+    foreach (var s in Enumerable.Range(0, stacks.Length))
+    {
+        var item = lines[y][4 * s + 1];
+        if (item != ' ')
+        {
+            stacks[s].Push(item);
+        }
+    }
+}
+
+var regex = new Regex("move (?<count>\\d+) from (?<from>\\d+) to (?<to>\\d+)", RegexOptions.Compiled);
+foreach (var instruction in lines.Skip(height + 2))
+{
+    var match = regex.Match(instruction);
+    var count = int.Parse(match.Groups["count"].Value);
+    var from = int.Parse(match.Groups["from"].Value) - 1;
+    var to = int.Parse(match.Groups["to"].Value) - 1;
+    for (var box = 0; box < count; box++)
+    {
+        stacks[to].Push(stacks[from].Pop());
+    }
+}
+
+Console.WriteLine(stacks.Select(_ => _.Peek()).ToArray());
