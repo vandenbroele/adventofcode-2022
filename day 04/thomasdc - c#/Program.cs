a2c52ac5547445ab2711c3e29d3b79fdd6eb2d9b
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-05 21:48:51 +0100

    thomasdc day 4

---
+++ day 04/thomasdc - c#/Program.cs
@@ -0,0 +1,24 @@
+﻿var sections = (
+    from line in File.ReadAllLines("input.txt")
+    let split = line.Split(',')
+    let left = split[0].Split('-')
+    let leftLower = int.Parse(left[0])
+    let leftUpper = int.Parse(left[1])
+    let right = split[1].Split('-')
+    let rightLower = int.Parse(right[0])
+    let rightUpper = int.Parse(right[1])
+    select (left: (lower: leftLower, upper: leftUpper), right: (lower: rightLower, upper: rightUpper))).ToArray();
+
+Console.WriteLine((
+        from s in sections
+        where (s.left.lower <= s.right.lower && s.left.upper >= s.right.upper) ||
+              (s.right.lower <= s.left.lower && s.right.upper >= s.left.upper)
+        select s)
+    .Count());
+
+Console.WriteLine((
+        from s in sections
+        where (s.left.upper >= s.right.lower && s.left.lower <= s.right.upper) ||
+              (s.right.upper >= s.left.lower && s.right.lower <= s.left.upper)
+        select s)
+    .Count());
