332b6bd9029cddae2b2b161a502c0cc68808d357
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-06 17:56:50 +0100

    thomasdc day 6

---
+++ day 06/thomasdc - c#/Program.cs
@@ -0,0 +1,7 @@
+﻿var stream = File.ReadAllText("input.txt");
+const int window = 14;
+Console.WriteLine((
+    from x in Enumerable.Range(window, stream.Length)
+    let sub = stream[(x - window)..x]
+    where sub.Distinct().Count() == window
+    select (x, sub)).First());
