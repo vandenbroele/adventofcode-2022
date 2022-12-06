193151468c23d55651ff61007e9de3d8f2f801a7
mrgelmir <sander.decleer@novado.be>
2022-12-06 06:23:51 +0100

    sanderdc Day6

---
+++ day 06/sanderdc - c#/Day6/Program.cs
@@ -0,0 +1,32 @@
+﻿string input = File.ReadAllText("./input.txt");
+
+
+int indexPart1 = FindMarkerIndex(input, 4);
+Console.WriteLine($"part 1: {indexPart1}");
+
+int indexPart2 = FindMarkerIndex(input, 14);
+Console.WriteLine($"part 2: {indexPart2}");
+
+int FindMarkerIndex(string s, int markerLength)
+{
+    HashSet<char> buffer = new(markerLength);
+
+    int currentIndex = markerLength - 1;
+    for (; currentIndex < s.Length; currentIndex++)
+    {
+        buffer.Clear();
+
+        for (int i = currentIndex - markerLength + 1; i <= currentIndex; i++)
+        {
+            buffer.Add(s[i]);
+        }
+
+        if (buffer.Count == markerLength)
+        {
+            Console.WriteLine(string.Join("", buffer));
+            break;
+        }
+    }
+
+    return currentIndex + 1;
+}
