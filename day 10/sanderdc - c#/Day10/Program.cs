fa09b6020b2f0ce285d85cb42c1f77c9c6f64d74
mrgelmir <sander.decleer@novado.be>
2022-12-10 09:41:06 +0100

    sanderdc Day10 cleanup

--- day 10/sanderdc - c#/Day10/Program.cs
+++ day 10/sanderdc - c#/Day10/Program.cs
@@ -1,9 +1,5 @@
 ﻿using IEnumerator<string> commandEnumerator = File.ReadLines("./input.txt").GetEnumerator();
 
-// test should be 13140
-
-// using IEnumerator<string> commandEnumerator = File.ReadLines("./testInput.txt").GetEnumerator();
-
 int cycle = 0;
 int register = 1;
 
@@ -17,7 +13,6 @@ List<char> display = new(20 * 6);
 while (hasNext)
 {
     string command = commandEnumerator.Current;
-    string[] commands = command.Split(' ');
 
     cycle++;
 
@@ -25,19 +20,16 @@ while (hasNext)
     if ((cycle - 20) % 40 == 0)
     {
         int cycleScore = cycle * register;
-
         score += cycleScore;
-        // Console.WriteLine($"at cycle {cycle} X is {register} resulting in {cycleScore} (total: {score})");
     }
 
     // Part 2
     int rowPos = (cycle - 1) % 40;
-
-    bool overlapsSprite = rowPos >= register-1 && rowPos <= register + 1;
+    bool overlapsSprite = rowPos >= register - 1 && rowPos <= register + 1;
 
     display.Add(overlapsSprite ? '#' : '.');
 
-
+    string[] commands = command.Split(' ');
     switch (commands[0])
     {
         case "noop":
@@ -57,23 +49,19 @@ while (hasNext)
     }
 }
 
-Console.WriteLine(score);
+Console.WriteLine($"part 1: {score}");
+Console.WriteLine("part 2:");
+RenderDisplay(display);
 
-// Write display
-for (int i = 0; i < display.Count; i++)
+void RenderDisplay(IReadOnlyList<char> chars)
 {
-    if (i % 40 == 0)
+    for (int i = 0; i < chars.Count; i++)
     {
-        Console.WriteLine();
+        if (i % 40 == 0)
+        {
+            Console.WriteLine();
+        }
+
+        Console.Write(chars[i]);
     }
-
-    Console.Write(display[i]);
 }
-
-// output
-// ###..####.#..#.#....###...##..#..#..##..
-// #..#....#.#..#.#....#..#.#..#.#..#.#..#.
-// #..#...#..#..#.#....###..#..#.#..#.#..#.
-// ###...#...#..#.#....#..#.####.#..#.####.
-// #....#....#..#.#....#..#.#..#.#..#.#..#.
-// #....####..##..####.###..#..#..##..#..#.
