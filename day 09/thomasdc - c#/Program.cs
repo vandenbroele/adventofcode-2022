12d14f5470b60409432e0637af6e0f1a7621050d
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-11 14:46:47 +0100

    thomasdc day 9 part 2

--- day 09/thomasdc - c#/Program.cs
+++ day 09/thomasdc - c#/Program.cs
@@ -1,33 +1,66 @@
-﻿var visited = new HashSet<Position> { new() };
+﻿Console.WriteLine(Solve(File.ReadAllLines("input.txt"), 1));
+Console.WriteLine(Solve(File.ReadAllLines("input.txt"), 9));
 
-Knot tail = new(new(), new());
-Knot head = new(new(), new());
-
-foreach (var instruction in File.ReadAllLines("input.txt"))
+int Solve(string[] instructions, int ropeLength)
 {
-    var direction = instruction[0];
-    var stepCount = int.Parse(instruction[2..]);
+    var visited = new HashSet<Position> { new() };
 
-    foreach (var _ in Enumerable.Range(0, stepCount))
+    var head = new Knot { Current = new(), Previous = new() };
+    var knots = new Knot[ropeLength];
+    for (var i = 0; i < knots.Length; i++)
     {
-        head = new Knot(direction switch
+        knots[i] = new Knot
         {
-            'U' => head.Current with { Y = head.Current.Y - 1 },
-            'D' => head.Current with { Y = head.Current.Y + 1 },
-            'L' => head.Current with { X = head.Current.X - 1 },
-            'R' => head.Current with { X = head.Current.X + 1 },
-            _ => head.Current
-        }, head.Current);
+            Current = new(), 
+            Previous = new(),
+            NextKnot = i == 0 ? head : knots[i - 1]
+        };
+    }
 
-        if (!head.Current.IsTouching(tail.Current))
+    foreach (var instruction in instructions)
+    {
+        var direction = instruction[0];
+        var stepCount = int.Parse(instruction[2..]);
+
+        foreach (var _ in Enumerable.Range(0, stepCount))
         {
-            tail = new(head.Previous, tail.Current);
-            visited.Add(tail.Current);
+            head.Previous = head.Current;
+            head.Current = direction switch
+            {
+                'U' => head.Current with { Y = head.Current.Y - 1 },
+                'D' => head.Current with { Y = head.Current.Y + 1 },
+                'L' => head.Current with { X = head.Current.X - 1 },
+                'R' => head.Current with { X = head.Current.X + 1 },
+                _ => head.Current
+            };
+
+            for (var i = 0; i < knots.Length; i++)
+            {
+                var knot = knots[i];
+                if (!knot.NextKnot!.Current.IsTouching(knot.Current))
+                {
+                    var nextPosition = knot.Current.Diff(knot.NextKnot.Current) switch
+                    {
+                        (2, 1 or -1) or (2, 0) => knot.NextKnot.Current with { Y = knot.NextKnot.Current.Y + 1 },
+                        (-2, 1 or -1) or (-2, 0) => knot.NextKnot.Current with { Y = knot.NextKnot.Current.Y - 1 },
+                        (1 or -1, 2) or (0, 2) => knot.NextKnot.Current with { X = knot.NextKnot.Current.X + 1 },
+                        (1 or -1, -2) or (0, -2) => knot.NextKnot.Current with { X = knot.NextKnot.Current.X - 1 },
+                        _ => knot.NextKnot!.Previous
+                    };
+
+                    knot.Previous = knot.Current;
+                    knot.Current = nextPosition;
+                    if (i == knots.Length - 1)
+                    {
+                        visited.Add(knot.Current);
+                    }
+                }
+            }
         }
     }
-}
 
-Console.WriteLine(visited.Count);
+    return visited.Count;
+}
 
 public record Position(int Y = 0, int X = 0)
 {
@@ -37,6 +70,12 @@ public record Position(int Y = 0, int X = 0)
         select (y, x)).ToArray();
 
     public bool IsTouching(Position p) => NeighbourDiffs.Any(_ => p.Y == Y + _.yDiff && p.X == X + _.xDiff);
+    public (int y, int x) Diff(Position p) => (Y - p.Y, X - p.X);
 }
 
-public record Knot(Position Current, Position Previous);
+public class Knot
+{
+    public required Position Current { get; set; }
+    public required Position Previous { get; set; }
+    public Knot? NextKnot { get; init; }
+}
