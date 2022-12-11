2166309681f980b1138d0ae11fffacb8e6a1458d
thomasdc <1496708+thomasdc@users.noreply.github.com>
2022-12-11 21:38:23 +0100

    thomasdc day 10 part 1 refactored

--- day 10/thomasdc - c#/Program.cs
+++ day 10/thomasdc - c#/Program.cs
@@ -1,35 +1,33 @@
-﻿int cycleNumber;
-var nextCycleNumber = 1;
-int x;
+﻿var x = 0;
 var nextX = 1;
 var signalStrength = 0;
-foreach (var line in File.ReadAllLines("input.txt"))
+var nextCycleNumber = 1;
+var lines = File.ReadAllLines("input.txt");
+var lineI = 0;
+for (var cycleNumber = 1; cycleNumber <= 240; cycleNumber++)
 {
-    x = nextX;
-    cycleNumber = nextCycleNumber;
+    if (cycleNumber == nextCycleNumber)
+    {
+        x = nextX;
 
-    Console.WriteLine($"cycle {cycleNumber} starting at {cycleNumber} and ending {cycleNumber + 1} has value {x} in the middle");
+        nextX = lines[lineI][..4] switch
+        {
+            "addx" => x + int.Parse(lines[lineI][5..]),
+            _ => x
+        };
+
+        nextCycleNumber = lines[lineI][..4] switch
+        {
+            "addx" => cycleNumber + 2,
+            _ => cycleNumber + 1
+        };
+
+        lineI++;
+    }
 
     if (cycleNumber is 20 or 60 or 100 or 140 or 180 or 220)
     {
         signalStrength += cycleNumber * x;
-    }
-
-    nextX = line[..4] switch
-    {
-        "addx" => x + int.Parse(line[5..]),
-        _ => x
-    };
-
-    nextCycleNumber = line[..4] switch
-    {
-        "addx" => cycleNumber + 2,
-        _ => cycleNumber + 1
-    };
-
-    if (nextCycleNumber is 21 or 61 or 101 or 141 or 181 or 221 && cycleNumber is 19 or 59 or 99 or 139 or 179 or 219)
-    {
-        signalStrength += (cycleNumber + 1) * x;
     }
 }
 
