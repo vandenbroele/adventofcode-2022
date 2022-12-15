99db614062ddf147b11bbaa183f2f4e3bd10bc3c
mrgelmir <sander.decleer@novado.be>
2022-12-15 19:33:47 +0100

    sanderdc Day 15 part 2

--- day 15/sanderdc - c#/Day15/Program.cs
+++ day 15/sanderdc - c#/Day15/Program.cs
@@ -1,24 +1,29 @@
-﻿string[] inputs = { "./input.txt", "./testInput.txt" };
-List<string> inputLines = File.ReadLines(inputs[0]).ToList();
+﻿Input[] inputs =
+{
+    new("./testInput.txt", 10, 20),
+    new("./input.txt", 2000000, 4000000)
+};
 
-// const int rowToCheck = 10;
-const int rowToCheck = 2000000;
+(string path, int rowToCheck, int maxSize) = inputs[1];
 
-List<(Pos sensorPos, Pos beaconPos, int distance)> pairs = inputLines.Select(ParseLine).ToList();
+List<string> inputLines = File.ReadLines(path).ToList();
 
+List<(Pos sensorPos, Pos beaconPos, int distance)> pairs = inputLines
+    .Select(ParseLine)
+    .ToList();
 
 HashSet<int> positions = new();
 
 foreach ((Pos sensorPos, Pos _, int distance) in pairs)
 {
-    (int from, int to) range = GetRowOverlapRange(rowToCheck, sensorPos, distance);
+    Range range = GetRowOverlapRange(rowToCheck, sensorPos, distance);
 
-    if (range.to - range.from <= 0)
+    if (range.To - range.From <= 0)
     {
         continue;
     }
 
-    for (int i = range.from; i <= range.to; i++)
+    for (int i = range.From; i <= range.To; i++)
     {
         positions.Add(i);
     }
@@ -32,32 +37,123 @@ foreach ((Pos _, Pos beaconPos, int _) in pairs)
     }
 }
 
-Console.WriteLine(positions.Count);
+Console.WriteLine($"part 1: {positions.Count}");
 
+
+List<Range> ranges = new();
+for (int row = 0; row <= maxSize; row++)
+{
+    ranges.Clear();
+
+    foreach ((Pos sensorPos, Pos _, int distance) in pairs)
+    {
+        Range range = GetRowOverlapRange(row, sensorPos, distance);
+
+        if (range.To - range.From <= 0)
+        {
+            continue;
+        }
+
+
+        int min = Math.Max(range.From, 0);
+        int max = Math.Min(range.To, maxSize);
+
+        CombineRanges(ranges, new Range(min, max));
+    }
+
+    if (ranges.Count > 1)
+    {
+        int column = ranges[0].To + 1;
+        long result = column * 4000000L + row;
+        Console.WriteLine($"part 2: {result}");
+    }
+}
+
+void CombineRanges(List<Range> list, Range range)
+{
+    if (list.Count <= 0)
+    {
+        list.Add(range);
+        return;
+    }
+
+    // Assume ordered
+
+    for (int i = 0; i < list.Count; i++)
+    {
+        Range other = list[i];
+
+        if (other.To < range.From)
+            continue;
+
+        if (other.From > range.To)
+        {
+            list.Insert(i, range);
+            return;
+        }
+
+        // Enveloped
+        if (range.From >= other.From && range.To <= other.To)
+        {
+            return;
+        }
+
+
+        // Append
+        if (range.From < other.From && range.To >= other.From && range.To <= other.To)
+        {
+            list[i] = new Range(range.From, other.To);
+            return;
+        }
+
+        if (range.From >= other.From && range.From <= other.To && range.To > other.To)
+        {
+            Range combinedRange = new(other.From, range.To);
+            list.RemoveAt(i);
+
+            // Check further overlap
+            CombineRanges(list, combinedRange);
+            return;
+        }
+
+
+        // Full overlap
+        if (other.From > range.From && other.To < range.To)
+        {
+            // Check further overlap
+
+            list.RemoveAt(i);
+            CombineRanges(list, range);
+            return;
+        }
+    }
+
+
+    if (list[^1].To < range.From)
+    {
+        list.Add(range);
+        return;
+    }
+
+    Console.WriteLine($"what to do? {String.Join(", ", list)} <= {range}");
+}
 
 int Distance(Pos sensorPos1, Pos beaconPos1)
 {
     return Math.Abs(sensorPos1.X - beaconPos1.X) + Math.Abs(sensorPos1.Y - beaconPos1.Y);
 }
 
-(int from, int to) GetRowOverlapRange(int rowY, Pos sensor, int distance)
+
+Range GetRowOverlapRange(int rowY, Pos sensor, int distance)
 {
     int yDist = Math.Abs(sensor.Y - rowY);
     if (yDist > distance)
     {
-        return (0, 0);
+        return new Range(0, 0);
     }
 
     int offset = distance - yDist;
-    return (sensor.X - offset, sensor.X + offset);
-}
-
-int GetRowOverlapCount(int rowY, Pos sensor, int distance)
-{
-    int yDist = Math.Abs(sensor.Y - rowY);
-    int maxRow = 1 + 2 * distance;
-
-    return Math.Max(0, maxRow - 2 * yDist);
+    return new Range(sensor.X - offset, sensor.X + offset);
 }
 
 (Pos sensorPos, Pos beaconPos, int distance) ParseLine(string line)
@@ -99,3 +195,7 @@ IEnumerable<int> GetNumbers(string line)
 
 
 public record Pos(int X, int Y);
+
+internal record Range(int From, int To);
+
+internal record Input(string Path, int RowToCheck, int MaxSize);
