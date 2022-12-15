90ff9da293ce2778cfcb5fa30557671bb95ba8c5
mrgelmir <sander.decleer@novado.be>
2022-12-15 20:06:48 +0100

    sanderdc Day16 prep

---
+++ day 16/sanderdc - c#/Day16/Program.cs
@@ -0,0 +1,11 @@
+﻿Input[] inputs =
+{
+    new("./testInput.txt"),
+    new("./input.txt")
+};
+
+Input input = inputs[0];
+List<string> inputLines = File.ReadLines(input.Path).ToList();
+
+
+internal record Input(string Path);
