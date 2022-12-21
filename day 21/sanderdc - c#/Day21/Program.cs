25c3a4492d27cab806369a4c0e9501ae5ac476ec
mrgelmir <sander.decleer@novado.be>
2022-12-21 10:04:12 +0100

    sanderdc Day 21 part 2

--- day 21/sanderdc - c#/Day21/Program.cs
+++ day 21/sanderdc - c#/Day21/Program.cs
@@ -17,36 +17,109 @@ foreach (string inputLine in inputLines)
 
     if (int.TryParse(operation, out int number))
     {
-        monkeys.Add(name, new NumberMonkey(number));
+        if (name == "humn")
+        {
+            monkeys.Add(name, new HumanMonkey(number));
+        }
+        else
+        {
+            monkeys.Add(name, new NumberMonkey(number));
+        }
     }
     else
     {
         string[] operands = operation.Split(' ');
-        monkeys.Add(name, new MathMonkey(
-            operands[1] switch
-            {
-                "+" => MathMonkey.Operation.Add,
-                "-" => MathMonkey.Operation.Subtract,
-                "*" => MathMonkey.Operation.Multiply,
-                "/" => MathMonkey.Operation.Divide,
-                _ => throw new Exception()
-            },
-            new FetchMonkey(operands[0], GetMonkey),
-            new FetchMonkey(operands[2], GetMonkey)));
+        MathMonkey.Operation math = operands[1] switch
+        {
+            "+" => MathMonkey.Operation.Add,
+            "-" => MathMonkey.Operation.Subtract,
+            "*" => MathMonkey.Operation.Multiply,
+            "/" => MathMonkey.Operation.Divide,
+            _ => throw new Exception()
+        };
+        FetchMonkey left = new FetchMonkey(operands[0], GetMonkey);
+        FetchMonkey right = new FetchMonkey(operands[2], GetMonkey);
+
+
+        if (name == "root")
+        {
+            monkeys.Add(name, new RootMonkey(math, left, right));
+        }
+        else
+        {
+            monkeys.Add(name, new MathMonkey(math, left, right));
+        }
     }
 }
 
 IMonkey GetMonkey(string name) => monkeys[name];
 
-long result = GetMonkey("root").GetResult();
+IMonkey rootMonkey = GetMonkey("root");
+long result = rootMonkey.GetResult();
 Console.WriteLine(result);
 
-internal record Input(string Path);
+rootMonkey.Reverse(-1);
+HumanMonkey human = GetMonkey("humn") as HumanMonkey;
+Console.WriteLine(human.Result);
 
+internal record Input(string Path);
 
 public interface IMonkey
 {
     public long GetResult();
+    public bool HasHuman();
+    public void Reverse(long number);
+}
+
+public class HumanMonkey : IMonkey
+{
+    private readonly long initialNumber;
+    public long Result { get; private set; } = -1;
+
+    public HumanMonkey(long initialNumber)
+    {
+        this.initialNumber = initialNumber;
+    }
+
+    public long GetResult() => initialNumber;
+
+    public bool HasHuman() => true;
+
+    public void Reverse(long number) => Result = number;
+}
+
+public class RootMonkey : IMonkey
+{
+    private readonly IMonkey left;
+    private readonly IMonkey right;
+
+    private MathMonkey mathMonkey;
+
+    public RootMonkey(MathMonkey.Operation operation, IMonkey left, IMonkey right)
+    {
+        this.left = left;
+        this.right = right;
+
+        mathMonkey = new MathMonkey(operation, left, right);
+    }
+
+    public long GetResult() => mathMonkey.GetResult();
+
+    public bool HasHuman() => true;
+
+    public void Reverse(long number)
+    {
+        if (left.HasHuman())
+        {
+            long desiredResult = right.GetResult();
+            left.Reverse(desiredResult);
+        }
+        else
+        {
+            long desiredResult = left.GetResult();
+            right.Reverse(desiredResult);
+        }
+    }
 }
 
 public class FetchMonkey : IMonkey
@@ -61,6 +134,9 @@ public class FetchMonkey : IMonkey
     }
 
     public long GetResult() => getter(name).GetResult();
+    public bool HasHuman() => getter(name).HasHuman();
+
+    public void Reverse(long number) => getter(name).Reverse(number);
 }
 
 public class NumberMonkey : IMonkey
@@ -73,6 +149,11 @@ public class NumberMonkey : IMonkey
     }
 
     public long GetResult() => number;
+    public bool HasHuman() => false;
+
+    public void Reverse(long _)
+    {
+    }
 }
 
 public class MathMonkey : IMonkey
@@ -88,7 +169,6 @@ public class MathMonkey : IMonkey
         this.right = right;
     }
 
-
     public long GetResult()
     {
         return operation switch
@@ -99,6 +179,38 @@ public class MathMonkey : IMonkey
             Operation.Divide => left.GetResult() / right.GetResult(),
             _ => throw new Exception()
         };
+    }
+
+    public bool HasHuman() => left.HasHuman() || right.HasHuman();
+
+    public void Reverse(long number)
+    {
+        if (left.HasHuman())
+        {
+            long rightResult = right.GetResult();
+
+            left.Reverse(operation switch
+            {
+                Operation.Add => number - rightResult,
+                Operation.Subtract => number + rightResult,
+                Operation.Multiply => number / rightResult,
+                Operation.Divide => number * rightResult,
+                _ => throw new ArgumentOutOfRangeException()
+            });
+        }
+        else
+        {
+            long leftResult = left.GetResult();
+
+            right.Reverse(operation switch
+            {
+                Operation.Add => number - leftResult,
+                Operation.Subtract => leftResult - number,
+                Operation.Multiply => number / leftResult,
+                Operation.Divide => leftResult / number,
+                _ => throw new ArgumentOutOfRangeException()
+            });
+        }
     }
 
     public enum Operation
