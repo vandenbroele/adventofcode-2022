ef4e97346732ce3146ce8826f88444ad4daa4047
mrgelmir <sander.decleer@novado.be>
2022-12-22 22:21:46 +0100

    sanderdc Day 22 part 1

--- day 22/sanderdc - c#/Day22/Program.cs
+++ day 22/sanderdc - c#/Day22/Program.cs
@@ -4,7 +4,7 @@
     new("./input.txt")
 };
 
-Input input = inputs[0];
+Input input = inputs[1];
 
 List<string> allLines = File.ReadLines(input.Path).ToList();
 
@@ -15,6 +15,58 @@ Character character = new(grid.GetStartPos(), Orientation.Right);
 
 grid.Render(character);
 
+Execute(commandString);
+
+Console.WriteLine();
+grid.Render(character);
+
+int col = character.Position.Col + 1;
+int row = grid.Height - character.Position.Row;
+int orientation = ScoreOrientation(character.Orientation);
+Console.WriteLine($"part 1 : {1000 * row + 4 * col + orientation}");
+
+
+void Execute(string commands)
+{
+    using CharEnumerator charEnumerator = commands.GetEnumerator();
+    string numberString = "";
+    while (charEnumerator.MoveNext())
+    {
+        if (char.IsNumber(charEnumerator.Current))
+        {
+            numberString += charEnumerator.Current;
+        }
+
+        else
+        {
+            if (numberString.Length > 0)
+            {
+                int moveAmount = int.Parse(numberString);
+                Position newPos = grid.Move(character.Position, character.Orientation, moveAmount);
+                character.Position = newPos;
+
+                numberString = "";
+            }
+
+            switch (charEnumerator.Current)
+            {
+                case 'L':
+                    character.RotateLeft();
+                    break;
+                case 'R':
+                    character.RotateRight();
+                    break;
+            }
+        }
+    }
+
+    if (numberString.Length > 0)
+    {
+        int moveAmount = int.Parse(numberString);
+        Position newPos = grid.Move(character.Position, character.Orientation, moveAmount);
+        character.Position = newPos;
+    }
+}
 
 Grid Parse(IList<string> gridLines)
 {
@@ -40,9 +92,21 @@ Grid Parse(IList<string> gridLines)
     return new Grid(width, height, positions);
 }
 
+int ScoreOrientation(Orientation orientation) => orientation switch
+{
+    Orientation.Right => 0,
+    Orientation.Down => 1,
+    Orientation.Left => 2,
+    Orientation.Up => 3,
+    _ => throw new ArgumentOutOfRangeException()
+};
+
 internal class Grid
 {
     private readonly Tile[,] tiles;
+
+    public int Width => tiles.GetLength(0);
+    public int Height => tiles.GetLength(1);
 
     public Grid(int width, int height, List<(Position, Tile)> positions)
     {
@@ -62,13 +126,88 @@ internal class Grid
         }
     }
 
+    public Position Move(Position position, Orientation direction, int amount)
+    {
+        Position result = position;
+        Position currentPos = position;
+
+        for (int xOffset = 1; xOffset <= amount; xOffset++)
+        {
+            currentPos = NextTileInDirection(currentPos, direction);
+
+            Tile nextTile = tiles[currentPos.Col, currentPos.Row];
+            if (nextTile == Tile.Solid)
+            {
+                break;
+            }
+
+            result = currentPos;
+        }
+
+        return result;
+    }
+
+    private Position NextTileInDirection(Position start, Orientation orientation)
+    {
+        Position next = orientation switch
+        {
+            Orientation.Right => start with { Col = start.Col + 1 },
+            Orientation.Left => start with { Col = start.Col - 1 },
+            Orientation.Up => start with { Row = start.Row + 1 },
+            Orientation.Down => start with { Row = start.Row - 1 },
+            _ => start
+        };
+
+        if (next.Row >= Height)
+        {
+            next = next with { Row = 0 };
+        }
+
+        if (next.Row < 0)
+        {
+            next = next with { Row = Height - 1 };
+        }
+
+        if (next.Col >= Width)
+        {
+            next = next with { Col = 0 };
+        }
+
+        if (next.Col < 0)
+        {
+            next = next with { Col = Width - 1 };
+        }
+
+        // Handle open
+        if (tiles[next.Col, next.Row] == Tile.Empty)
+        {
+            next = NextTileInDirection(next, orientation);
+        }
+
+        return next;
+    }
+
+    public Position GetStartPos()
+    {
+        int row = tiles.GetLength(1) - 1;
+        for (int col = 0; col < tiles.GetLength(0); col++)
+        {
+            if (tiles[col, row] == Tile.Open)
+            {
+                return new Position(col, row);
+            }
+        }
+
+        throw new Exception("No start pos found");
+    }
+
     public void Render(Character character)
     {
         for (int row = tiles.GetLength(1) - 1; row >= 0; row--)
         {
             for (int col = 0; col < tiles.GetLength(0); col++)
             {
-                if (character.Position.X == col && character.Position.Y == row)
+                if (character.Position.Col == col && character.Position.Row == row)
                 {
                     Console.Write(character.Orientation switch
                     {
@@ -92,31 +231,36 @@ internal class Grid
             Console.WriteLine();
         }
     }
-
-    public Position GetStartPos()
-    {
-        int row = tiles.GetLength(1) - 1;
-        for (int col = 0; col < tiles.GetLength(0); col++)
-        {
-            if (tiles[col, row] == Tile.Open)
-            {
-                return new Position(col, row);
-            }
-        }
-
-        throw new Exception("No start pos found");
-    }
 }
 
-internal record Character(Position Position, Orientation Orientation)
+internal class Character
 {
-    public void RotateLeft()
+    public Position Position { get; set; }
+    public Orientation Orientation { get; private set; }
+
+    public Character(Position position, Orientation orientation)
     {
+        Position = position;
+        Orientation = orientation;
     }
 
-    public void RotateRight()
+    public void RotateLeft() => Orientation = Orientation switch
     {
-    }
+        Orientation.Left => Orientation.Down,
+        Orientation.Down => Orientation.Right,
+        Orientation.Right => Orientation.Up,
+        Orientation.Up => Orientation.Left,
+        _ => Orientation.Right,
+    };
+
+    public void RotateRight() => Orientation = Orientation switch
+    {
+        Orientation.Left => Orientation.Up,
+        Orientation.Up => Orientation.Right,
+        Orientation.Right => Orientation.Down,
+        Orientation.Down => Orientation.Left,
+        _ => Orientation.Right,
+    };
 }
 
 internal enum Tile
@@ -136,4 +280,4 @@ internal enum Orientation
 
 internal record Input(string Path);
 
-internal record Position(int X, int Y);
+internal record Position(int Col, int Row);
